4abf919a8f724daec637655162827b7e6a2e9541
leerobertdyer <leerobertdyer@gmail.com>
2024-05-10 21:55:52 -0400

    home page responsive styling

--- src/Components/Button/buttonVariants.ts
+++ src/Components/Button/buttonVariants.ts
@@ -2,15 +2,15 @@ import { FormEvent } from 'react'
 import { tv, type VariantProps } from 'tailwind-variants'
 
 export const button = tv({
-	base: 'py-2 px-4 mt-2 mb-2 rounded-[2rem] h-[2.75rem] flex justify-center items-center border-none font-bold cursor-pointer transition-all duration-300 ease-in-out',
+	base: 'py-2 px-4 mt-2 mb-2 rounded-[8px] h-[2.75rem] flex justify-center items-center font-bold cursor-pointer transition-all duration-300 ease-in-out',
 	variants: {
 		role: {
 			primary: 'bg-wabsPurple text-white hover:bg-[#a5a5a5] hover:text-black border-[1px]',
-			secondary: 'bg-gray text-black hover:bg-black hover:text-gray border-[1px]',
+			secondary: 'bg-gray text-wabsPurple hover:bg-wabsGray hover:border-none hover:text-black border-[1px] border border-2 border-wabsPurple',
 		},
 		size: {
-			small: 'text-sm w-[6rem] h-[2.875rem]',
-			medium: 'text-[.875rem] w-[80%] h-[2.875rem]',
+			small: 'text-sm w-[7rem]',
+			medium: 'text-[.875rem] w-[80%] m-auto',
 			full: 'w-full',
 		},
 	},
--- src/Views/Home/Home.tsx
+++ src/Views/Home/Home.tsx
@@ -4,11 +4,11 @@ import Button from "../../Components/Button/Button";
 
 export default function Home() {
   return (
-    <div className="bg-white p-4 pb-[8rem] flex flex-col items-center ">
+    <div className="bg-white p-4 pb-[8rem] md:px-[15%] flex flex-col ">
       {/* Mobile Header: We can't all be Mozart... */}
-      <div className="sm:hidden">
+      <div className="sm:hidden flex flex-col items-center">
         <div
-          className="overflow-hidden h-[343px] w-w[343px] flex flex-col justify-end items-center"
+          className="overflow-hidden rounded-md h-[343px] w-w[343px] flex flex-col justify-end items-center"
           style={{
             backgroundImage: 'url("/mozartcolory.png")',
             backgroundSize: "cover",
@@ -31,56 +31,63 @@ export default function Home() {
         </div>
       </div>
       {/* Desktop Header: We can't all be Mozart... */}
-      <div className="flex justify-between p-2 w-full  text-3xl text-wabsSecondary">
-        <div className="w-[26rem]">
-          <h1>We can't all be Mozart,</h1>
-          <p className="pb-2">
-            Let's write a <span className="line-through">bad</span> song.
-          </p>
-          <p className="text-[1rem]">
-            Write a <span className="crossed">bad</span>
-            song this month to keep your friends and yourself accountable.
-          </p>
-          <div className="w-full flex justify-between">
-            <Link to="/login">
-            <Button role="primary" size="small">
-              Log In
-            </Button>
-            </Link>
-            <Link to="/signup">
-              <button className="text-white bg-wabsPurple rounded-lg p-2 w-[6rem] text-[1rem]">
-                Sign Up
-              </button>
-            </Link>
+      <div className="hidden sm:block">
+        <div className="flex justify-between p-2 w-full text-3xl text-wabsSecondary">
+          <div className="w-[20rem] flex flex-col justify-between p-4">
+            <h1>
+              We can't all be Mozart,
+              <p className="pb-2">
+                Let's write a <span className="line-through">bad</span> song.
+              </p>
+            </h1>
+            <p className="text-[1rem]">
+              Write a <span className="crossed">bad</span>
+              song this month to keep your friends and yourself accountable.
+            </p>
+            <div className="w-full flex justify-between">
+              <Link to="/login">
+                <Button role="primary" size="small">
+                  Log In
+                </Button>
+              </Link>
+              <Link to="/signup">
+                <Button role="secondary" size="small">
+                  Sign Up
+                </Button>
+              </Link>
+            </div>
           </div>
-        </div>
 
-        <div
-          className="overflow-hidden h-[343px] w-[343px]"
-          style={{
-            backgroundImage: 'url("/mozartcolory.png")',
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        />
+          <div
+            className="overflow-hidden rounded-md h-[310px] w-[310px]"
+            style={{
+              backgroundImage: 'url("/mozartcolory.png")',
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          />
+        </div>
       </div>
-      <div className="p-5 flex flex-col align-center justify-center">
-        <p className="text-2xl pt-2">
-          Writing is a muscle...
-          <br />
-          And bad songs are the weights that create metaphorical six packs.
-        </p>
-        <p className="text-xs pt-2 text-[#737373]">
-          Write a bad song, so when a good song comes you're ready!
-        </p>
+      {/* Writing Is A Muscle Mobile */}
+      <div className="p-5 flex flex-col align-center justify-center w-full sm:hidden">
+        <div>
+          <p className="text-2xl pt-2">
+            Writing is a muscle...
+            <br />
+            And bad songs are the weights that create metaphorical six packs.
+          </p>
+          <p className="text-xs pt-2 text-[#737373]">
+            Write a bad song, so when a good song comes you're ready!
+          </p>
+        </div>
         <div
-          className="overflow-hidden h-[235px] min-h-[13rem] mt-4 w-full flex flex-col  justify-start items-center"
+          className="overflow-hidden rounded-md h-[235px] min-h-[13rem] mt-4 w-full flex flex-col  justify-start items-center"
           style={{
             backgroundImage: 'url("/musicians1.png")',
             backgroundSize: "cover",
             backgroundPosition: "center",
           }}
-        ></div>
+        />
         <div className="w-[22rem] py-3 h-fit">
           <Link to="/submit-song">
             <Button role="primary" size="full">
@@ -88,7 +95,42 @@ export default function Home() {
             </Button>
           </Link>
         </div>
-        <div className="flex flex-col">
+      </div>
+
+      {/* Writing Is A Muscle DESKTOP */}
+      <div className="hidden sm:block">
+        <div className="flex align-center justify-center w-full">
+          <div
+            className="h-[15rem] rounded-md mt-4 w-full"
+            style={{
+              backgroundImage: 'url("/musicians1.png")',
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          />
+          <div className="flex flex-col justify-center items-center text-center">
+            <p className="text-xl pt-2">
+              Writing is a muscle...
+              <br />
+              And bad songs are the weights that create metaphorical six packs.
+            </p>
+            <p className="text-xs pt-2 text-wabsGray">
+              Write a bad song, so when a good song comes you're ready!
+            </p>
+            <div className="w-[22rem] py-3 h-fit">
+              <Link to="/submit-song">
+                <Button role="primary" size="medium">
+                  Write
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-between w-full"></div>
+      </div>
+      {/* Reviews Start */}
+      <div className="flex">
+        <div className="flex flex-col-reverse p-5 sm:w-[45vw]">
           <div className="flex mt-2">
             <FaStar size={23} fill="#0075B2" />
             <FaStar size={23} fill="#0075B2" />
@@ -100,7 +142,7 @@ export default function Home() {
             It’s the best app ever!!!! Writing one song a month keeps me focused
             and consistent!
           </p>
-          <div className="flex items-center pt-2 gap-3 h-[50px]">
+          <div className="flex items-center gap-3">
             <div
               className="rounded-full h-[50px] w-[50px]"
               style={{
@@ -108,13 +150,44 @@ export default function Home() {
                 backgroundSize: "cover",
                 backgroundPosition: "center",
               }}
-            ></div>
+            />
             <div>
-              <p className="text-sm">Regina Miles</p>
               <p className="text-sm text-[#252B42]">Guitarist</p>
+              <p className="text-sm">Regina Miles</p>
             </div>
           </div>
         </div>
+        {/* Review 1 End */}
+        {/* Review 2 Start */}
+
+        <div className="hidden sm:flex flex-col-reverse p-5 w-[45vw]">
+          <div className="flex mt-2">
+            <FaStar size={23} fill="#0075B2" />
+            <FaStar size={23} fill="#0075B2" />
+            <FaStar size={23} fill="#0075B2" />
+            <FaStar size={23} fill="#0075B2" />
+            <FaRegStar size={23} fill="#0075B2" />
+          </div>
+          <p className="text-wabsSecondary text-xl pt-2">
+            It’s the best app ever!!!! Writing one song a month keeps me focused
+            and consistent!
+          </p>
+          <div className="flex items-center gap-3">
+            <div
+              className="rounded-full h-[50px] w-[50px]"
+              style={{
+                backgroundImage: 'url("/mozartcolory.png")',
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+            />
+            <div>
+              <p className="text-sm text-[#252B42]">Guitarist</p>
+              <p className="text-sm">Regina Miles</p>
+            </div>
+          </div>
+        </div>
+        {/* Review 2End */}
       </div>
     </div>
   );
